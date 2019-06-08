366258e91319e2273530615f90713312c419742b
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-06-07 21:16:32 -0300

    Fix: layout do menu quebrado

--- app/src/main/java/com/cubos/android/MainActivity.kt
+++ app/src/main/java/com/cubos/android/MainActivity.kt
@@ -3,8 +3,10 @@ package com.cubos.android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.Menu
+import android.view.inputmethod.EditorInfo
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,8 +19,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
 
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // adapter.getFilter().filter(newText)
+                return false
+            }
+        })
         return true
     }
 }

49929484da171f114bb8483b0970846b3a8b8594
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-06-07 21:00:34 -0300

    adição de botão para pesquisa

--- app/src/main/java/com/cubos/android/MainActivity.kt
+++ app/src/main/java/com/cubos/android/MainActivity.kt
@@ -3,8 +3,8 @@ package com.cubos.android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +14,11 @@ class MainActivity : AppCompatActivity() {
 
         val gridLayoutManager = GridLayoutManager(applicationContext, 2)
         recyclerView.layoutManager = gridLayoutManager
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+
+        return true
     }
 }
